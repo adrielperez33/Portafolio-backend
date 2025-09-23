82b203c4660bec5f0fa1452a0956b6ed6ee625ea
Adriel Pérez <adrielperez227@ejemplo.com>
2025-09-23 17:35:24 -0300

    express y node

--- HolaMundo.js
+++ HolaMundo.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("👋 Hola Mundo desde Render con Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
